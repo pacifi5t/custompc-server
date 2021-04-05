57e030aae0eff593fd8e4bf1ed1ad0d53f222dfd
Andrey Dergach <thequasar7@gmail.com>
2021-04-06 01:49:43 +0300

    Connected DB, tweaked tsconfig.json

--- index.ts
+++ index.ts
@@ -1,14 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import db from 'db';
 
 const PORT = process.env.PORT || 9999;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 async function start() {
   try {
+    await db.authenticate();
+    await db.sync();
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (er) {
-    console.log(er);
+    console.error(er);
   }
 }
 
---
+++ src/db.ts
@@ -0,0 +1,24 @@
+import { Sequelize } from 'sequelize';
+
+if (!process.env.DB_NAME) {
+  throw new Error('Wrong DB_NAME');
+}
+
+if (!process.env.DB_USER) {
+  throw new Error('Wrong DB_USER');
+}
+
+if (!process.env.DB_PORT) {
+  throw new Error('Wrong DB_PORT');
+}
+
+export default new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT)
+  }
+);
